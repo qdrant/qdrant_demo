d3dac2ca8b433394bd741f608b4626b2a3687ed3
Andrey Vasnetsov <andrey@vasnetsov.com>
2021-08-01 13:26:33 +0200

    add host config to neural searcher

--- qdrant_demo/neural_searcher.py
+++ qdrant_demo/neural_searcher.py
@@ -4,13 +4,15 @@ from qdrant_client import QdrantClient
 from qdrant_openapi_client.models.models import Filter
 from sentence_transformers import SentenceTransformer
 
+from qdrant_demo.config import QDRANT_HOST, QDRANT_PORT
+
 
 class NeuralSearcher:
 
     def __init__(self, collection_name: str):
         self.collection_name = collection_name
         self.model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens', device='cpu')
-        self.qdrant_client = QdrantClient()
+        self.qdrant_client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
 
     def search(self, text: str, filter_: dict = None) -> List[dict]:
         vector = self.model.encode(text).tolist()
